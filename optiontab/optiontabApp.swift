4887e6858e1eb6c5a546d176e946455f93e83c62
Andrea Mario Lufino <andrea@andrealufino.com>
2026-04-18 21:29:42 +0200

    Initial Commit

---
+++ optiontab/optiontabApp.swift
@@ -0,0 +1,17 @@
+//
+//  optiontabApp.swift
+//  optiontab
+//
+//  Created by Andrea Mario Lufino on 18/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct optiontabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
